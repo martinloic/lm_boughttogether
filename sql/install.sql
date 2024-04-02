a2cfd7e7ad8cf56293d68677c8e5069cfecbcd79
Loïc MARTIN <martin.loic.prod@gmail.com>
2024-04-02 10:16:57 +0200

    Fixing install.sql

--- sql/install.sql
+++ sql/install.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS `PREFIXlm_product_bought_together` (
     `id_association` INT(10) unsigned NOT NULL AUTO_INCREMENT,
     `product_id` INT(10) unsigned  NOT NULL,
     `associated_product_id` INT(10) unsigned NOT NULL,
+    `occurrences` INT(10) unsigned NOT NULL,
     PRIMARY KEY (`id_association`)
     -- occurrences INT,
 ) ENGINE=ENGINE_TYPE  DEFAULT CHARSET=utf8;
